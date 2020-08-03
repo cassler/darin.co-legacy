68b9723ca693e476ea5092bdf9cb37f15b537ea6
Darin Cassler <Darin.Cassler@coxautoinc.com>
2020-08-03 12:30:24 -0500

    ff to include react app building

--- packages/react-ts/src/App.tsx
+++ packages/react-ts/src/App.tsx
@@ -2,25 +2,28 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { Worfklower } from '@wf/core'
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.tsx</code> and save to reload.
+					{/* {JSON.stringify(partner_settings)} */}
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
         </a>
-      </header>
-    </div>
-  );
+			</header>
+		</div>
+	);
 }
 
 export default App;
