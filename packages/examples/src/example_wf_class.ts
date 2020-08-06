7f546af021fb00bf24ac859d53fc4ed922afde02
Darin Cassler <Darin.Cassler@coxautoinc.com>
2020-08-06 08:25:01 -0500

    fix input problems with example script

--- packages/examples/src/example_wf_class.ts
+++ packages/examples/src/example_wf_class.ts
@@ -1,7 +1,7 @@
 
 import { Workflower } from '@wf/core';
 import { getJSONfromSpreadsheet } from '@wf/csv'; // Utility for easy file handling
-import { settings } from '@wf/react-ts';
+import { partner_settings } from './partner_settings';
 // This is our configurations file where aspects of the implementations are described
 
 /**
@@ -13,7 +13,7 @@ export function wf_examples(partner, options) {
 
 	// Allow entire sdt of partner configs to be passed,
 	// but narrow down that set to the one we want.
-	let config = options;
+	let config = options.find(i => i.partner === partner);
 
 	// To be passed into our new Workflower instance.
 	let props = {
@@ -56,4 +56,4 @@ export function wf_examples(partner, options) {
 
 }
 
-wf_examples("BOA", settings.boa)
+wf_examples("BOA", partner_settings)
