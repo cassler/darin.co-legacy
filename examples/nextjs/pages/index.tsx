71d54ee126772eabb943034d3a7e71feb92a4bb1
Darin Cassler <Darin.Cassler@coxautoinc.com>
2020-08-12 00:00:46 -0500

    update lerna for pub

--- examples/nextjs/pages/index.tsx
+++ examples/nextjs/pages/index.tsx
@@ -1,4 +1,12 @@
 import React from "react";
 import { Button } from "@cassler/components";
 
-export default () => <Button />;
+export default () => (
+	<div>
+		<Button />
+		<Button />
+		<Button />
+		<Button />
+	</div>
+)
+
