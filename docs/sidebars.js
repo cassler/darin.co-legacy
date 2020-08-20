f14d6b075d1c084c67e980ce450442c702d09d2d
Darin Cassler <Darin.Cassler@coxautoinc.com>
2020-08-20 08:39:27 -0500

    add docs back to main repo. deploy to gh-pages, doesnt need submodule

---
+++ docs/sidebars.js
@@ -0,0 +1,36 @@
+module.exports = {
+  someSidebar: {
+
+		Overview: [
+			"intro",
+			"guide_partner_select",
+			"guide_choose_dt",
+			"guide_partner_submission",
+			"guide_set_exclusions",
+			"guide_preview",
+			"guide_results"
+		] ,
+		"Output Guide": [
+			"process_state",
+			"calculate_changes",
+		],
+		"Supported Partners": [
+			"boa",
+			"drw",
+			"cnz",
+			"haz",
+			"das",
+			"goo"
+		],
+		"Further Reading": [
+			"methodology",
+			"product_subscription_emails",
+		]
+		// "Partner Support": [
+
+		// 	"cnz",
+		// 	"goo",
+		// 	"das"
+		// ]
+  },
+};
