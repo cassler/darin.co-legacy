af05be0ac33ff95b33d2a0f0ceff9db5638892a8
Darin Cassler <Darin.Cassler@coxautoinc.com>
2020-08-22 12:36:29 -0500

    add styles

--- packages/components/src/button.tsx
+++ packages/components/src/button.tsx
@@ -1,12 +1,24 @@
 /* eslint-disable no-alert */
 import { meaningOfLife } from "@nighttrax/foo";
 import React from "react";
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core';
 
-export const Button = () => (
-  <button
-    type="button"
-    onClick={() => alert(`the meaning if life is ${meaningOfLife}`)}
-  >
-    Click me
-  </button>
-);
+export const Button = () => {
+	const styles = css`
+		padding: 10px;
+		border: none;
+		border-radius: 8px;
+		background-color: #339;
+		color: #ddf;
+	`
+	return (
+		<button
+			css={styles}
+			type="button"
+			onClick={() => alert(`the meaning if life is ${meaningOfLife}`)}
+		>
+			Click me!!!!!
+		</button>
+	)
+};
