af4d4744d9e0099148a4633d0f4e6733a41d035f
Darin Cassler <Darin.Cassler@coxautoinc.com>
2020-08-22 11:26:51 -0500

    fix more imports

--- packages/components/src/Page/Page.tsx
+++ packages/components/src/Page/Page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Header } from '../PageHeader/Header';
+import { Header } from '@cassler/components';
 // import './page.css';
 
 export interface PageProps {
--- packages/components/src/PageHeader/Header.tsx
+++ packages/components/src/PageHeader/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Button } from '../Button/button';
+import { Button } from '@cassler/components';
 // import './header.css';
 
 export interface HeaderProps {
