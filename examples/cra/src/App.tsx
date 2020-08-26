004684b07e89958ff81e8f6fd882487eb038e2f3
Darin Cassler <Darin.Cassler@coxautoinc.com>
2020-08-26 17:21:23 -0500

    update microsweeper

--- examples/cra/src/App.tsx
+++ examples/cra/src/App.tsx
@@ -1,7 +1,5 @@
 import React, { useState, useEffect, useMemo } from 'react';
-import { Button, LabLogo, List } from "@cassler/components";
-import { motion } from "framer-motion"
-import faker from 'faker';
+import { MicroSweeper } from '@cassler/components';
 
 function App() {
 
@@ -23,52 +21,9 @@ function App() {
 
 	return (
 		<div className="app-container">
-			<a href='' onClick={() => toggleDarkMode(!darkMode)}><LabLogo title="darin" /></a>
 			<div className="content">
-				<List header="header of the list" footer={(
-					<>
-						<Button label="Save Post" size="small" primary />&nbsp;
-						<Button label="Edit" size="small" />&nbsp;
-						<Button label="Next" size="small" ghost disabled />
-					</>
-				)}>
-					<p>Dolores perferendis qui illo impedit. Exercitationem laborum ad similique est molestiae iure rem culpa. Sunt quibusdam voluptatibus illum aliquid aut esse voluptatibus. Ut est sapiente id qui.</p>
-				</List>
-				<br />
-				<List header="header of the list" footer={(
-					<>
-						<Button label="New Post" size="small" disabled ghost />&nbsp;
-						<Button label="Edit" size="small" disabled ghost />&nbsp;
-						<Button label="Next" size="small" ghost disabled />
-					</>
-				)}>
-					<p>Dolores perferendis qui illo impedit. Exercitationem laborum ad similique est molestiae iure rem culpa. Sunt quibusdam voluptatibus illum aliquid aut esse voluptatibus. Ut est sapiente id qui.</p>
-				</List>
-				<motion.div
-					initial={{ scale: 1, y: 25, opacity: 0 }}
-					animate={{ scale: 1, y: 0, opacity: 1 }}
-					transition={{ ease: "easeOut", duration: 1, delay: .25 }}
-				>
-					<p className="muted">
-						Now let's use <strong>React's test renderer</strong> and Jest's snapshot feature to interact with the component and capture the rendered output and create a snapshot file:
-					</p>
-					<p className="sub">
-						Now let's use React's test renderer and Jest's snapshot feature to interact with the component and capture the rendered output and create a snapshot file:
-					</p>
-				</motion.div>
-
-				<motion.div
-					initial={{ scale: 1, y: 25, opacity: 0 }}
-					animate={{ scale: 1, y: 0, opacity: 1 }}
-					transition={{ ease: "easeOut", duration: 1, delay: 0.5 }}
-				>
-					<Button label="Source" primary />&nbsp;
-					<Button label="Find out more" />&nbsp;
-					<Button label="Let's talk" ghost={true} onClick={() => toggleDarkMode(!darkMode)} />
-				</motion.div>
-				<p className="body">
-					Now let's use React's test renderer and Jest's snapshot feature to interact with the component and capture the rendered output and create a snapshot file:
-		</p>
+				<h1><small>Lets play</small><br />MicroSweeper</h1>
+				<MicroSweeper size={20} />
 			</div>
 		</div>
 	)
