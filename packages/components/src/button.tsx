4f1084e851bc17c6cb383354e198f4a7206f0ee5
Darin Cassler <Darin.Cassler@coxautoinc.com>
2020-09-07 16:35:10 -0500

    switch to airbnb config of eslint

--- packages/components/src/button.tsx
+++ packages/components/src/button.tsx
@@ -1,55 +1,66 @@
 /* eslint-disable no-alert */
+import React from "react";
 import { colors } from "@cassler/color";
-import React, { ReactElement, ReactNode, ButtonHTMLAttributes } from "react";
 /** @jsx jsx */
-import { css, jsx } from '@emotion/core';
+import { css, jsx } from "@emotion/core";
 
-
-type Props = React.ButtonHTMLAttributes<HTMLButtonElement> & {
-	size?: 'small' | 'large',
-	primary?: boolean,
-	ghost?: boolean
+export interface IButtonProps {
+	children: React.ReactNode;
+	onClick: React.MouseEventHandler;
+	size?: "small" | "large" | "medium";
+	primary?: boolean;
+	ghost?: boolean;
 }
 
-export const Button: React.FC<Props> = ({ onClick, children, size, primary, ghost, className }) => {
+export function Button({
+	size, primary, ghost, onClick, children
+}: IButtonProps): JSX.Element {
 	const defaultStyle = css`
-		padding: 6px 12px;
-		border: 2px solid ${colors.gray[3]};
-		font-size: 13px;
-		font-weight: 600;
-		letter-spacing: -0.033em;
-		border-radius: 6px;
-		background-color: ${colors.gray[2]};
-		color: ${colors.gray[8]};
-		&:hover { cursor: pointer} ;
-	`
+    padding: 6px 12px;
+    border: 2px solid ${colors.gray[3]};
+    font-size: 13px;
+    font-weight: 600;
+    letter-spacing: -0.033em;
+    border-radius: 6px;
+    background-color: ${colors.gray[2]};
+    color: ${colors.gray[8]};
+    &:hover {
+      cursor: pointer;
+    }
+  `;
 	const primaryStyle = css`
-		background-color: ${colors.indigo[6]};
-		border-color: ${colors.indigo[8]};
-		color: ${colors.gray[0]};
-		&:hover { background-color: ${colors.indigo[8]}}
-	`
+    background-color: ${colors.indigo[6]};
+    border-color: ${colors.indigo[8]};
+    color: ${colors.gray[0]};
+    &:hover {
+      background-color: ${colors.indigo[8]};
+    }
+  `;
 	const ghostStyle = css`
 		background-color: transparent;
 		/* border: 1px solid ${colors.gray[4]}; */
-	`
-	const smallStyle = css`
-		padding: 4px 8px;
-		font-size: 12px;
 	`;
+	const smallStyle = css`
+    padding: 4px 8px;
+    font-size: 12px;
+  `;
 	const largeStyle = css`
-		padding: 12px 18px;
-	`
+    padding: 12px 18px;
+  `;
 	const styles = [
 		defaultStyle,
-		size === 'small' && smallStyle,
-		size === 'large' && largeStyle,
+		size === "small" && smallStyle,
+		size === "large" && largeStyle,
 		primary && primaryStyle,
-		ghost && ghostStyle
-	]
+		ghost && ghostStyle,
+	];
 	return (
-		<button className={className} type="button" css={styles} onClick={() => onClick ? onClick : null}>
+		<button
+			type="button"
+			css={styles}
+			onClick={() => onClick || null}
+		>
 			{children}
 		</button>
-	)
-};
+	);
+}
