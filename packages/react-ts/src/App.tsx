dc09af135ceb5150da957cbb50085444231e5beb
Darin Cassler <Darin.Cassler@coxautoinc.com>
2020-09-14 11:25:52 -0500

    remove unused consts

--- packages/react-ts/src/App.tsx
+++ packages/react-ts/src/App.tsx
@@ -1,157 +1,153 @@
-import React, { useState } from 'react';
-import './App.css';
-import ViewSettings from './components/App/ViewSettings';
-import { Dropdown, Modal, Layout, Card, Button, Divider } from 'antd';
-import { SettingOutlined, SwapOutlined } from '@ant-design/icons'
-import { motion, AnimatePresence } from "framer-motion"
-import WorkflowForm from './components/WorkflowForm';
-import Stepper from './components/App/Stepper';
-import SaveContextButton from './components/App/SaveContextButton';
-import PreferenceMenu from './components/App/PreferenceMenu';
-import { WFProvider, WFContext } from './context';
-import ResultsView from './components/ResultsView'
-import ReportViewer from './components/ReportViewer'
-
+import React, { useState } from "react";
+import "./App.css";
+import ViewSettings from "./components/App/ViewSettings";
+import { Modal, Layout, Card, Button, Divider } from "antd";
+import { SettingOutlined, SwapOutlined } from "@ant-design/icons";
+import { motion, AnimatePresence } from "framer-motion";
+import WorkflowForm from "./components/WorkflowForm";
+import Stepper from "./components/App/Stepper";
+import SaveContextButton from "./components/App/SaveContextButton";
+import { WFProvider, WFContext } from "./context";
+import ResultsView from "./components/ResultsView";
+import ReportViewer from "./components/ReportViewer";
 
 function App() {
-	function handleClick(e) {
-		console.log('click', e);
-	}
+  const [defaultMode, toggleMode] = useState<boolean>(false);
 
-	const [defaultMode, toggleMode] = useState<boolean>(false)
+  const layoutStyle = {
+    display: "grid",
+    gridTemplateColumns: defaultMode ? "300px 1fr" : "380px 1fr",
+    height: "100vh",
+    width: "100%",
+  };
+  const sideBarStyle = {
+    padding: "96px 48px 24px 24px",
+    display: "grid",
+    gridTemplateRows: "1fr min-content",
+  };
 
-	const layoutStyle = {
-		display: "grid",
-		gridTemplateColumns: defaultMode ? "300px 1fr" : "380px 1fr",
-		height: "100vh",
-		width: "100%",
-	}
-	const sideBarStyle = {
-		padding: '96px 48px 24px 24px',
-		display: "grid",
-		gridTemplateRows: "1fr min-content"
-	}
+  const toolbarStyle: React.CSSProperties = {
+    position: "fixed",
+    top: 0,
+    left: 0,
+    right: 0,
+    width: "100%",
+    background: "#fff",
+    display: "grid",
+    padding: "18px 24px",
+    gridTemplateColumns: "1fr 320px",
+    alignItems: "center",
+    boxShadow: "2px 2px 8px rgba(100, 100, 100, 0.1)",
+    zIndex: 500,
+  };
 
-	const toolbarStyle: React.CSSProperties = {
-		position: "fixed",
-		top: 0,
-		left: 0,
-		right: 0,
-		width: "100%",
-		background: '#fff',
-		display: 'grid',
-		padding: '18px 24px',
-		gridTemplateColumns: '1fr 320px',
-		alignItems: 'center',
-		boxShadow: '2px 2px 8px rgba(100, 100, 100, 0.1)',
-		zIndex: 500
-	}
+  const motionPrefs = {
+    transition: {
+      ease: "easeInOut",
+      duration: 0.3,
+      delay: 0,
+    },
+    initial: { y: -100, opacity: 0, scale: 0.7 },
+    animate: { y: 0, opacity: 1, scale: 1 },
+    exit: { y: 100, opacity: 0, scale: 3.7 },
+  };
 
-	const contentStyle = {
-		padding: '96px 24px 24px',
-	}
-
-	const motionPrefs = {
-		transition: {
-			ease: "easeInOut",
-			duration: 0.3,
-			delay: 0
-		},
-		initial: { y: -100, opacity: 0, scale: 0.7 },
-		animate: { y: 0, opacity: 1, scale: 1 },
-		exit: { y: 100, opacity: 0, scale: 3.7 }
-	}
-
-
-	return (
-
-		<WFProvider>
-			<Layout>
-				<div style={toolbarStyle}>
-					<h1 className='App-logo'>
-						Workflower <small>{defaultMode ? 'Core' : 'Delta'}</small>
-					</h1>
-					<div style={{ textAlign: 'right' }}>
-						<Button onClick={() => toggleMode(!defaultMode)}>
-							<SwapOutlined /> Switch Mode
-					</Button>
-						<Button size="small" type="link" href="https://pages.ghe.coxautoinc.com/Darin-Cassler/workflower-monorepo/" target="_blank">Docs</Button>
-					</div>
-				</div>
-				{defaultMode ? (
-
-
-					<WFContext.Consumer>
-						{({ ctx }) => (
-							<div style={layoutStyle}>
-								<>
-									<div style={sideBarStyle}>
-										<Stepper
-											partner={ctx.partner}
-											setStep={ctx.setStep}
-											refSize={ctx.reference?.data.length}
-											reqSize={ctx.requested?.data.length}
-											index={ctx.step} />
-										<div style={{ textAlign: 'center' }}>
-											<Button
-												key="m3"
-												disabled={!ctx.partner}
-												onClick={() => ctx.togglePartnerSettings(!ctx.showPartnerSettings)}
-												icon={<SettingOutlined />}
-												title="Partner Settings">
-												Partner Settings {ctx.partner && `(${ctx.partner})`}
-											</Button>
-											<Divider />
-											<SaveContextButton />
-										</div>
-									</div>
-									<AnimatePresence exitBeforeEnter>
-										{ctx.step <= 4 ? (
-											<motion.div {...motionPrefs} key="0">
-												<Card className='result-card'>
-													<WorkflowForm />
-												</Card>
-											</motion.div>
-										) : (
-												<motion.div {...motionPrefs} key="1">
-													<ResultsView
-														partner_name={ctx.partner_name}
-														partner={ctx.partner} log={ctx.log} result={ctx.result} liveCount={ctx.config.live_ids ? ctx.config.live_ids.length : 0} handleBack={() => {
-															ctx.setStep(Math.max(0, ctx.step - 1))
-														}}
-													/>
-												</motion.div>
-											)}
-									</AnimatePresence>
-									<Modal
-										title={`Partner Settings - ${ctx.partner}`}
-										width={800}
-										style={{ top: 24 }}
-										visible={ctx.showPartnerSettings && ctx.config}
-										onOk={() => ctx.togglePartnerSettings(false)}
-										onCancel={() => ctx.togglePartnerSettings(false)}
-									>
-										<ViewSettings config={ctx.config} />
-									</Modal>
-								</>
-							</div>
-						)}
-					</WFContext.Consumer>
-				) : (
-						<div style={{ ...layoutStyle }}>
-
-							<ReportViewer />
-
-						</div>
-					)}
-			</Layout>
-		</WFProvider>
-
-
-	);
+  return (
+    <WFProvider>
+      <Layout>
+        <div style={toolbarStyle}>
+          <h1 className="App-logo">
+            Workflower <small>{defaultMode ? "Core" : "Delta"}</small>
+          </h1>
+          <div style={{ textAlign: "right" }}>
+            <Button onClick={() => toggleMode(!defaultMode)}>
+              <SwapOutlined /> Switch Mode
+            </Button>
+            <Button
+              size="small"
+              type="link"
+              href="https://pages.ghe.coxautoinc.com/Darin-Cassler/workflower-monorepo/"
+              target="_blank"
+            >
+              Docs
+            </Button>
+          </div>
+        </div>
+        {defaultMode ? (
+          <WFContext.Consumer>
+            {({ ctx }) => (
+              <div style={layoutStyle}>
+                <>
+                  <div style={sideBarStyle}>
+                    <Stepper
+                      partner={ctx.partner}
+                      setStep={ctx.setStep}
+                      refSize={ctx.reference?.data.length}
+                      reqSize={ctx.requested?.data.length}
+                      index={ctx.step}
+                    />
+                    <div style={{ textAlign: "center" }}>
+                      <Button
+                        key="m3"
+                        disabled={!ctx.partner}
+                        onClick={() =>
+                          ctx.togglePartnerSettings(!ctx.showPartnerSettings)
+                        }
+                        icon={<SettingOutlined />}
+                        title="Partner Settings"
+                      >
+                        Partner Settings {ctx.partner && `(${ctx.partner})`}
+                      </Button>
+                      <Divider />
+                      <SaveContextButton />
+                    </div>
+                  </div>
+                  <AnimatePresence exitBeforeEnter>
+                    {ctx.step <= 4 ? (
+                      <motion.div {...motionPrefs} key="0">
+                        <Card className="result-card">
+                          <WorkflowForm />
+                        </Card>
+                      </motion.div>
+                    ) : (
+                      <motion.div {...motionPrefs} key="1">
+                        <ResultsView
+                          partner_name={ctx.partner_name}
+                          partner={ctx.partner}
+                          log={ctx.log}
+                          result={ctx.result}
+                          liveCount={
+                            ctx.config.live_ids ? ctx.config.live_ids.length : 0
+                          }
+                          handleBack={() => {
+                            ctx.setStep(Math.max(0, ctx.step - 1));
+                          }}
+                        />
+                      </motion.div>
+                    )}
+                  </AnimatePresence>
+                  <Modal
+                    title={`Partner Settings - ${ctx.partner}`}
+                    width={800}
+                    style={{ top: 24 }}
+                    visible={ctx.showPartnerSettings && ctx.config}
+                    onOk={() => ctx.togglePartnerSettings(false)}
+                    onCancel={() => ctx.togglePartnerSettings(false)}
+                  >
+                    <ViewSettings config={ctx.config} />
+                  </Modal>
+                </>
+              </div>
+            )}
+          </WFContext.Consumer>
+        ) : (
+          <div style={{ ...layoutStyle }}>
+            <ReportViewer />
+          </div>
+        )}
+      </Layout>
+    </WFProvider>
+  );
 }
 
-
-
 export default App;
-
--- packages/react-ts/src/components/ReportViewer.tsx
+++ packages/react-ts/src/components/ReportViewer.tsx
@@ -1,156 +1,165 @@
-import { ColumnsType } from 'antd/es/table';
-import React, { useState, useEffect } from 'react';
-import FileSelect from './IO/FileSelect';
-import { IParseResult } from '../context';
-import * as jsdiff from 'diff';
-import { Button, Table, Descriptions, Result, Divider, PageHeader, Drawer } from 'antd';
+import React, { useState, useEffect } from "react";
+import FileSelect from "./IO/FileSelect";
+import { IParseResult } from "../context";
+import * as jsdiff from "diff";
+import { Button, Result, Divider } from "antd";
 
-import { set, get } from 'idb-keyval';
-import { SwapOutlined, PlusCircleOutlined, MinusCircleOutlined } from '@ant-design/icons'
-import DeltaTable from './Preview/DeltaTable'
-import CSVTable, { CSVTableModal } from './Preview/CSVTable';
+import { set, get } from "idb-keyval";
+import {
+  SwapOutlined,
+  PlusCircleOutlined,
+  MinusCircleOutlined,
+} from "@ant-design/icons";
+import DeltaTable from "./Preview/DeltaTable";
+import CSVTable, { CSVTableModal } from "./Preview/CSVTable";
 
-export type DiffDataProps = IParseResult & { fileName: string }
+export type DiffDataProps = IParseResult & { fileName: string };
 
 const mapWithKeys = (data: any[]) => {
   return data.map((item, index) => ({
     ...item,
-    key: index
-  }))
-}
+    key: index,
+  }));
+};
 
 const alphaSort = (a, b) => {
-  let first = JSON.stringify(a)
-  let second = JSON.stringify(b)
-  if (first > second) { return -1 }
-  if (second > first) { return 1 }
+  let first = JSON.stringify(a);
+  let second = JSON.stringify(b);
+  if (first > second) {
+    return -1;
+  }
+  if (second > first) {
+    return 1;
+  }
   return 0;
-}
+};
 export function ReportViewer() {
   const [oldData, setOldData] = useState<DiffDataProps>();
   const [newData, setNewData] = useState<DiffDataProps>();
-  const [result, setResult] = useState<{
-    add: boolean,
-    removed: boolean,
-    count: number,
-    change: string,
-    value: string
-  }[]>([])
+  const [result, setResult] = useState<
+    {
+      add: boolean;
+      removed: boolean;
+      count: number;
+      change: string;
+      value: string;
+    }[]
+  >([]);
 
   const diffData = (oldObj: object[], newObj: object[]) => {
-    let oldStr = oldObj.map(i => JSON.stringify(i)).sort(alphaSort).join('\n')
-    let newStr = newObj.map(i => JSON.stringify(i)).sort(alphaSort).join('\n')
-    let raw = jsdiff.diffLines(oldStr, newStr)
-    let output = raw.filter(i => i.added || i.removed).map((type, tIndex) => {
-      let { added, removed } = type;
-      let entries = type.value.split('\n').filter(i => i.length > 5)
-      return entries.map((i, index) => ({
-        change: added ? <PlusCircleOutlined className='addition' /> : <MinusCircleOutlined className='deletion' />,
-        add: added ? true : false,
-        ...JSON.parse(i)
-      }))
-    })
-    console.log(output)
+    let oldStr = oldObj
+      .map((i) => JSON.stringify(i))
+      .sort(alphaSort)
+      .join("\n");
+    let newStr = newObj
+      .map((i) => JSON.stringify(i))
+      .sort(alphaSort)
+      .join("\n");
+    let raw = jsdiff.diffLines(oldStr, newStr);
+    let output = raw
+      .filter((i) => i.added || i.removed)
+      .map((type, tIndex) => {
+        let { added } = type;
+        let entries = type.value.split("\n").filter((i) => i.length > 5);
+        return entries.map((i, index) => ({
+          change: added ? (
+            <PlusCircleOutlined className="addition" />
+          ) : (
+            <MinusCircleOutlined className="deletion" />
+          ),
+          add: added ? true : false,
+          ...JSON.parse(i),
+        }));
+      });
+    console.log(output);
     // console.table(output[2])
     // console.table(output[5])
     return mapWithKeys(output.flat());
-  }
+  };
 
-  const handleChange = (result: IParseResult, slug: string, fileName: string) => {
-    if (slug === 'next') {
+  const handleChange = (
+    result: IParseResult,
+    slug: string,
+    fileName: string
+  ) => {
+    if (slug === "next") {
       let sortResult = {
         ...result,
-        data: result.data.sort((a, b) => { return a - b })
-      }
-      let newData = { ...sortResult, fileName }
+        data: result.data.sort((a, b) => {
+          return a - b;
+        }),
+      };
+      let newData = { ...sortResult, fileName };
 
-      setNewData(newData)
+      setNewData(newData);
       // setColumns(result.meta.fields)
-      set("nextCols", JSON.stringify(result.meta.fields))
-      set("nextData", JSON.stringify(newData))
+      set("nextCols", JSON.stringify(result.meta.fields));
+      set("nextData", JSON.stringify(newData));
     }
-    if (slug === 'prev') {
+    if (slug === "prev") {
       let sortResult = {
         ...result,
-        data: result.data.sort((a, b) => { return a - b })
-      }
-      let oldData = { ...sortResult, fileName }
-      setOldData(oldData)
+        data: result.data.sort((a, b) => {
+          return a - b;
+        }),
+      };
+      let oldData = { ...sortResult, fileName };
+      setOldData(oldData);
       // setColumns(result.meta.fields)
-      set("prevCols", JSON.stringify(result.meta.fields))
-      set("prevData", JSON.stringify(oldData))
+      set("prevCols", JSON.stringify(result.meta.fields));
+      set("prevData", JSON.stringify(oldData));
     }
-  }
+  };
 
   const resetCache = (type: string) => {
-    if (type === 'next') {
-      setNewData(undefined)
+    if (type === "next") {
+      setNewData(undefined);
       // setColumns([])
-      set("nextCols", null)
-      set("nextData", null)
+      set("nextCols", null);
+      set("nextData", null);
     }
-    if (type === 'prev') {
-
-      setOldData(undefined)
+    if (type === "prev") {
+      setOldData(undefined);
       // setColumns([])
-      set("prevCols", null)
-      set("prevData", null)
+      set("prevCols", null);
+      set("prevData", null);
     }
-  }
+  };
 
   const loadState = async () => {
-    const next = get<string>("nextData").then(data => {
-      setNewData(JSON.parse(data))
-    })
-    console.log(next)
-    const prev = get<string>("prevData").then(data => {
-      setOldData(JSON.parse(data))
-    })
-    console.log(prev)
-  }
+    const next = get<string>("nextData").then((data) => {
+      if (data) {
+        setNewData(JSON.parse(data));
+      }
+      return;
+    });
+    console.log(next);
+    const prev = get<string>("prevData").then((data) => {
+      if (data) {
+        setOldData(JSON.parse(data));
+      }
+      return;
+    });
+    console.log(prev);
+  };
 
   useEffect(() => {
-    loadState()
-  }, [])
+    loadState();
+  }, []);
 
   const contentStyle = {
-    padding: '96px 24px 24px',
-    width: '100vw',
-  }
+    padding: "96px 24px 24px",
+    width: "100vw",
+  };
   const deltaFileUI: React.CSSProperties = {
-    padding: '0 100px 0',
-    display: 'grid',
-    gridTemplateColumns: '1fr 100px 1fr',
-    alignItems: 'center',
-    justifyItems: 'center'
-  }
-
-  const makeTableCols = (payload: IParseResult, first?: number, filter?: string[]): ColumnsType[] => {
-    const fields = payload.meta.fields
-    const sample = payload.data[0];
-    const columns = fields.map((col, index) => ({
-      title: col.toUpperCase(),
-      dataIndex: col,
-      key: col.toLowerCase(),
-      ellipsis: true,
-      width: 30 + `${sample[col]}`.length * 5,
-      change: '',
-      sorter: (a, b) => a[col] - b[col],
-      sortDirections: ['ascend', 'descend'],
-      defaultSortOrder: ['ascend'],
-      render: (text, record) => (
-        <div style={{ wordWrap: 'break-word', wordBreak: 'break-word' }}>
-          {text}
-        </div>
-      ),
-    })).filter(i => filter ? !filter.includes(i.title) : true)
-
-    return first ? columns.slice(0, first) : columns
-  }
+    padding: "0 100px 0",
+    display: "grid",
+    gridTemplateColumns: "1fr 100px 1fr",
+    alignItems: "center",
+    justifyItems: "center",
+  };
 
   return (
-
-
     <div style={contentStyle}>
       <div>
         {result.length === 0 ? (
@@ -158,48 +167,85 @@ export function ReportViewer() {
             <Result
               status={oldData ? "success" : "info"}
               title="Previous File Added"
-              subTitle={oldData && oldData.fileName ? oldData.fileName : 'No file added'}
+              subTitle={
+                oldData && oldData.fileName ? oldData.fileName : "No file added"
+              }
               extra={[
-                !oldData && <FileSelect label="Old DT Report" slug="prev" callback={handleChange} />,
-                oldData && <div style={{ width: 200 }}>
-                  <Button key="buy" onClick={() => resetCache('prev')}>Reset</Button>&nbsp;
-									<CSVTableModal payload={oldData} filename={oldData.fileName} />
-                </div>
+                !oldData && (
+                  <FileSelect
+                    label="Old DT Report"
+                    slug="prev"
+                    callback={handleChange}
+                  />
+                ),
+                oldData && (
+                  <div style={{ width: 200 }}>
+                    <Button key="buy" onClick={() => resetCache("prev")}>
+                      Reset
+                    </Button>
+                    &nbsp;
+                    <CSVTableModal
+                      payload={oldData}
+                      filename={oldData.fileName}
+                    />
+                  </div>
+                ),
               ]}
             />
-            <div style={{ textAlign: 'center' }}>
-              <SwapOutlined style={{ fontSize: '48px', color: !oldData || !newData ? '#ccc' : '#333' }} />
+            <div style={{ textAlign: "center" }}>
+              <SwapOutlined
+                style={{
+                  fontSize: "48px",
+                  color: !oldData || !newData ? "#ccc" : "#333",
+                }}
+              />
               <Divider />
               <Button
                 type="primary"
                 disabled={!oldData || !newData}
                 size="large"
-                onClick={() => setResult(diffData(oldData.data, newData.data))}>
+                onClick={() => setResult(diffData(oldData.data, newData.data))}
+              >
                 Compare
-								</Button>
+              </Button>
             </div>
 
             <Result
               status={newData ? "success" : "info"}
               title="New File Added"
-              subTitle={newData && newData.fileName ? newData.fileName : 'No file added'}
+              subTitle={
+                newData && newData.fileName ? newData.fileName : "No file added"
+              }
               extra={[
-                !newData && <FileSelect label="New DT Report" slug="next" callback={handleChange} />,
-                newData && <div style={{ width: 200 }}>
-                  <Button key="buy" onClick={() => resetCache('next')}>Reset</Button>&nbsp;
-									<CSVTableModal filename={newData.fileName} payload={newData} />
-                </div>
+                !newData && (
+                  <FileSelect
+                    label="New DT Report"
+                    slug="next"
+                    callback={handleChange}
+                  />
+                ),
+                newData && (
+                  <div style={{ width: 200 }}>
+                    <Button key="buy" onClick={() => resetCache("next")}>
+                      Reset
+                    </Button>
+                    &nbsp;
+                    <CSVTableModal
+                      filename={newData.fileName}
+                      payload={newData}
+                    />
+                  </div>
+                ),
               ]}
             />
-
           </div>
-        ) : <DeltaTable data={result} onReset={() => setResult([])} />}
+        ) : (
+          <DeltaTable data={result} onReset={() => setResult([])} />
+        )}
         {oldData && <CSVTable filename={oldData.fileName} payload={oldData} />}
       </div>
     </div>
-
-  )
+  );
 }
-
 
 export default ReportViewer;
