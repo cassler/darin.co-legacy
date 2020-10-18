688ab0a754597ff2ae1b3cec53597fba62f9a106
Darin Cassler <Darin.Cassler@coxautoinc.com>
2020-10-18 12:47:19 -0500

    add hooks

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -1,57 +1,6 @@
-import { useRef, useState, useEffect } from "react";
-
-export function useWindowSize() {
-  // Initialize state with undefined width/height so server and client renders match
-  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
-  const [windowSize, setWindowSize] = useState({
-    width: undefined,
-    height: undefined,
-  });
-
-  useEffect(() => {
-    // Handler to call on window resize
-    function handleResize() {
-      // Set window width/height to state
-      setWindowSize({
-        width: window.innerWidth,
-        height: window.innerHeight,
-      });
-      let vh = window.innerHeight * 0.01;
-      document.documentElement.style.setProperty("--vh", `${vh}px`);
-    }
-
-    // Add event listener
-    window.addEventListener("resize", handleResize);
-
-    // Call handler right away so state gets updated with initial window size
-    handleResize();
-
-    // Remove event listener on cleanup
-    return () => window.removeEventListener("resize", handleResize);
-  }, []); // Empty array ensures that effect is only run on mount
-
-  return windowSize;
-}
-
-// Hook
-export function useMemoCompare(next, compare) {
-  // Ref for storing previous value
-  const previousRef = useRef();
-  const previous = previousRef.current;
-
-  // Pass previous and next value to compare function
-  // to determine whether to consider them equal.
-  const isEqual = compare(previous, next);
-
-  // If not equal update previousRef to next value.
-  // We only update if not equal so that this hook continues to return
-  // the same old value if compare keeps returning true.
-  useEffect(() => {
-    if (!isEqual) {
-      previousRef.current = next;
-    }
-  });
-
-  // Finally, if equal then return the previous value
-  return isEqual ? previous : next;
-}
+export * as useWindowSize from './useWindowSize';
+export * as useMemoCompare from './useMemoCompare';
+export * as usDarkMode from './useDarkMode';
+export * as useLocalStorage from './useLocalStorage';
+export * as useMedia from './useMedia'
+export * as useColorMode from './useColorMode';
