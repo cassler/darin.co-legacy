5afb085151126d64e0f47dda8b88964f74a380ee
Darin Cassler <darin@cassler.net>
2021-01-01 17:47:27 -0600

    finish implementing double quotes

--- packages/eslint-config/packages/base/index.js
+++ packages/eslint-config/packages/base/index.js
@@ -1,24 +1,24 @@
-const testOverrides = require('./tests.js');
+const testOverrides = require("./tests.js");
 
 module.exports = {
   root: true,
 
   extends: [
-    'eslint-config-airbnb-base/rules/best-practices',
-    'eslint-config-airbnb-base/rules/errors',
-    'eslint-config-airbnb-base/rules/node',
-    'eslint-config-airbnb-base/rules/style',
-    'eslint-config-airbnb-base/rules/variables',
-    'eslint-config-airbnb-base/rules/es6',
+    "eslint-config-airbnb-base/rules/best-practices",
+    "eslint-config-airbnb-base/rules/errors",
+    "eslint-config-airbnb-base/rules/node",
+    "eslint-config-airbnb-base/rules/style",
+    "eslint-config-airbnb-base/rules/variables",
+    "eslint-config-airbnb-base/rules/es6",
   ]
     .map(require.resolve)
     .concat([
       // This disables all stylistic rules from the above.
-      'prettier',
-      'prettier/react',
+      "prettier",
+      "prettier/react",
     ]),
 
-  parser: 'babel-eslint',
+  parser: "babel-eslint",
   parserOptions: {
     ecmaVersion: 2018,
     ecmaFeatures: {
@@ -32,74 +32,74 @@ module.exports = {
     jest: true,
   },
 
-  plugins: ['prettier', 'import'],
+  plugins: ["prettier", "import"],
 
   rules: {
-    'arrow-body-style': [2, 'as-needed'],
+    "arrow-body-style": [2, "as-needed"],
 
-    'import/no-extraneous-dependencies': [
-      'error',
+    "import/no-extraneous-dependencies": [
+      "error",
       {
         devDependencies: false,
       },
     ],
 
-    'jsx-a11y/href-no-hash': 'off',
+    "jsx-a11y/href-no-hash": "off",
     // "jsx-a11y/anchor-is-valid": 2,
-    'one-var': 0,
-    'one-var-declaration-per-line': 0,
-    'prefer-arrow-callback': 0,
+    "one-var": 0,
+    "one-var-declaration-per-line": 0,
+    "prefer-arrow-callback": 0,
     strict: 0,
-    'no-use-before-define': [2, { functions: false }],
-    'no-underscore-dangle': 0,
-    'no-param-reassign': [
+    "no-use-before-define": [2, { functions: false }],
+    "no-underscore-dangle": 0,
+    "no-param-reassign": [
       2,
       {
         props: false,
       },
     ],
-    'no-plusplus': 0,
-    'no-return-assign': ['error', 'except-parens'],
-    'no-restricted-syntax': [
+    "no-plusplus": 0,
+    "no-return-assign": ["error", "except-parens"],
+    "no-restricted-syntax": [
       2,
-      'ForInStatement',
-      'LabeledStatement',
-      'WithStatement',
+      "ForInStatement",
+      "LabeledStatement",
+      "WithStatement",
     ],
-    'no-shadow': [
+    "no-shadow": [
       2,
       {
-        hoist: 'all',
-        allow: ['resolve', 'reject', 'done', 'next', 'err', 'error'],
+        hoist: "all",
+        allow: ["resolve", "reject", "done", "next", "err", "error"],
       },
     ],
-    'no-unused-expressions': [
+    "no-unused-expressions": [
       2,
       {
         allowTaggedTemplates: true,
       },
     ],
-    'no-unused-vars': 2,
-    'prefer-const': [
-      'error',
+    "no-unused-vars": 2,
+    "prefer-const": [
+      "error",
       {
-        destructuring: 'all',
+        destructuring: "all",
       },
     ],
 
     quotes: [
-      'error',
-      'double',
+      "error",
+      "double",
       {
         avoidEscape: true,
         allowTemplateLiterals: true,
       },
     ],
-    'jsx-quotes': ['error', 'prefer-double'],
-    'prettier/prettier': [
-      'error',
+    "jsx-quotes": ["error", "prefer-double"],
+    "prettier/prettier": [
+      "error",
       {
-        trailingComma: 'es5',
+        trailingComma: "es5",
         printWidth: 80,
         singleQuote: false,
         jsxSingleQuote: false,
