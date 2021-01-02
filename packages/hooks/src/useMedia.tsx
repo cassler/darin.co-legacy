77b1936b6dbaa3fb8a58dd04849addd11ee439ae
Darin Cassler <darin@cassler.net>
2021-01-01 18:39:39 -0600

    move usemedia forward

--- packages/hooks/src/useMedia.tsx
+++ packages/hooks/src/useMedia.tsx
@@ -5,16 +5,6 @@ import { useState, useEffect } from "react";
  * @param queries
  * @param values
  * @param Example usage:
-
- const columnCount = useMedia<number>(
-    // Media queries
-    ['(min-width: 1500px)', '(min-width: 1000px)', '(min-width: 600px)'],
-    // Column counts (relates to above media queries by array index)
-    [5, 4, 3],
-    // Default column count
-    2
-  );
-
  * */
 
 export default function useMedia<T>(
